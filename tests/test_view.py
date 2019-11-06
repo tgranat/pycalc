715755bb93e9bccb3ae0abcee66c7748e97abd5e
Torbjorn Granat <torbjorn.granat@gmail.com>
2019-11-06 17:13:04 +0100

    Test adding

--- tests/test_view.py
+++ tests/test_view.py
@@ -6,10 +6,25 @@ from pycalc.__main__ import evaluateExpression
 def test_button(qtbot):
     # Setup
     window = PyCalcUi()
-    window.show()
+    #window.show()
     model = evaluateExpression
     PyCalcCtrl(model=model, view=window)
     qtbot.addWidget(window)
 
     qtbot.mouseClick(window.buttons['5'], Qt.LeftButton)
     assert window.display.text() == '5'
+
+def test_calc(qtbot):
+    # Setup
+    window = PyCalcUi()
+    window.show()
+    model = evaluateExpression
+    PyCalcCtrl(model=model, view=window)
+    qtbot.addWidget(window)
+    
+    qtbot.mouseClick(window.buttons['5'], Qt.LeftButton)
+    qtbot.mouseClick(window.buttons['+'], Qt.LeftButton)
+    qtbot.mouseClick(window.buttons['6'], Qt.LeftButton)
+    qtbot.mouseClick(window.buttons['='], Qt.LeftButton)
+    assert window.display.text() == '11'
+
